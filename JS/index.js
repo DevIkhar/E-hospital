8bce7312102aaaa0b5a33630457d107ec9d954ff
Devashish Ikhar <110417033+DevIkhar@users.noreply.github.com>
2023-10-26 01:49:35 -1200

    first commit

---
+++ JS/index.js
@@ -0,0 +1,25 @@
+// Header Scroll
+
+// let nav = document.querySelector(".navbar");
+// window.onscroll = function(){
+//     if(document.doucumentElement.scrollTop > 100){
+//         nav.classList.add("header_scrolled");
+//     }
+//     else{
+//         nav.classList.remove("header_scrolled");
+//     }
+// }
+
+
+// Hide nav
+
+// let navBar = document.querySelectorAll(".nav-link");
+// let navCollapse = document.getElementById("navbarNav");
+// navBar.forEach(function(a){
+//     a.addEventListener("click", function(){
+//         navCollapse.classList.remove("show");
+//     })
+// })  
+
+
+
